6cdff2c112d18dc897a1ae12f051bcab3d59e71e
AndMJ <andrejoao070@gmail.com>
2024-04-24 20:20:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Box, Typography} from "@mui/material";
+
+
+function App() {
+
+  return (
+    <>
+      <Box>
+          <Typography>André João</Typography>
+      </Box>
+    </>
+  )
+}
+
+export default App
