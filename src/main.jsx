39b50a0fcbccf72f7d8afc95232bf2e7071d94d9
AndMJ <andrejoao070@gmail.com>
2024-04-24 23:47:11 +0100

    site under construction

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import UnderConst from "./underConstruction/UnderConst.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        {/*<App />*/}
+        <UnderConst />
+    </React.StrictMode>,
 )
