2287b55b1ee090b48ba4dfdb99776a4f4e28a031
AndMJ <andrejoao070@gmail.com>
2024-04-25 17:24:58 +0100

    UPDATE template

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Box, Button, Container, IconButton, Typography, useMediaQuery} from "@mui/material";
+import {Box, Button, Container, Fab, Grid, Icon, IconButton, Typography, useMediaQuery} from "@mui/material";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -27,26 +27,60 @@ function App() {
 
     return (
         <ThemeProvider theme={Theme}>
-            <CssBaseline />
+            <CssBaseline enableColorScheme />
 
-            <Container maxWidth="lg" component={"section"} alignContent={"center"}>
-                <Box display={"flex"} position={"absolute"} top={0} right={0}>
-                    <IconButton onClick={() => setMode((current) => (current === 'light' ? 'dark' : 'light'))}>
-                        <ContrastIcon></ContrastIcon>
-                    </IconButton>
-                </Box>
+            {/* Button toggle light/dark mode */}
+            <Box position={"fixed"} bottom={10} right={10}>
+                <IconButton onClick={() => setMode((current) => (current === 'light' ? 'dark' : 'light'))}>
+                    <ContrastIcon></ContrastIcon>
+                </IconButton>
+            </Box>
+
+            <Container maxWidth="lg" component={"main"} alignContent={"center"}>
                 <Box height={"100vh"} alignContent={"center"}>
-                    <Typography variant={"h2"}>Hi, my name is <span style={{fontWeight: "bold"}}>André João</span></Typography>
+                    <Typography variant={"h2"}>Hi, my name is <span style={{fontWeight: "bold", color: "#0392EA"}}>André João</span></Typography>
                     <Typography variant={"h2"}>I'm a Web Developer <span style={{fontSize: 14}}>or try to.</span></Typography>
                 </Box>
             </Container>
 
-            <Container component={"section"}>
-                <Box height={"100vh"} alignContent={"center"}>
-                    <Typography variant={"h3"} >Resume</Typography>
-                    <Typography variant={"body1"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Typography>
+            {/*<Container component={"section"}>
+                <Box height={"100vh"} display={"flex"}>
+                    <Box alignContent={"center"} height={"100%"} style={{rotate: "-90deg"}}>
+                        <Typography variant={"h3"} >Resume</Typography>
+                    </Box>
+                    <Box alignContent={"center"}>
+
+                        <Typography variant={"body1"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Typography>
+                    </Box>
                 </Box>
-            </Container>
+            </Container>*/}
+
+            <Grid container spacing={2} height={"100vh"} mt={0}>
+                <Grid xs={2} alignContent={"start"} p={5}>
+                    <Typography variant={"h3"} style={{writingMode: "vertical-lr"}} position={"sticky"} top={30}>About</Typography>
+                </Grid>
+                <Grid xs={10} alignContent={"start"} p={5}>
+                    <Typography variant={"body1"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Typography>
+                </Grid>
+            </Grid>
+
+            <Grid container spacing={2} height={"100vh"} mt={0}>
+                <Grid xs={2} alignContent={"start"} p={5}>
+                    <Typography variant={"h3"} style={{writingMode: "vertical-lr"}} position={"sticky"} top={30}>Resume</Typography>
+                </Grid>
+                <Grid xs={10} alignContent={"start"} p={5}>
+                    <Typography variant={"body1"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Typography>
+                </Grid>
+            </Grid>
+
+            <Grid container spacing={2} height={"100vh"} mt={0}>
+                <Grid xs={2} alignContent={"start"} p={5}>
+                    <Typography variant={"h3"} style={{writingMode: "vertical-lr"}} position={"sticky"} top={30}>Projects</Typography>
+                </Grid>
+                <Grid xs={10} alignContent={"start"} p={5}>
+                    <Typography variant={"body1"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Typography>
+                </Grid>
+            </Grid>
 
         </ThemeProvider>
     )
