36ab889e27aba3ce11aa468901a07765fa93367f
AndMJ <andrejoao070@gmail.com>
2024-04-25 02:13:15 +0100

    UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
-import {Box, Button, Container, IconButton, Typography} from "@mui/material";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-import "@fontsource/montserrat"
-import "./App.css"
-import {useState} from "react";
-
-import ContrastIcon from '@mui/icons-material/Contrast';
 
 function App() {
 
-    const [changeTheme, setChangeTheme] = useState(true) //true = light, false = dark
-
-    const Theme = createTheme({
-        palette: {
-            mode: changeTheme ? "light" : "dark"
-        },
-        typography: {
-            fontFamily: [
-                'Montserrat',
-                'sans-serif',
-            ].join(','),
-        },
-    });
-
     return (
-        <ThemeProvider theme={Theme}>
-            <CssBaseline />
-            <Container>
-                <Box height={"100vh"} width={"100%"} alignContent={"center"} justifyContent={"center"} textAlign={"center"}>
-
-                </Box>
-            </Container>
-        </ThemeProvider>
+        <></>
     )
 }
 
