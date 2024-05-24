798ceec0ded370e43c035ab805c6be4c7e831ad3
AndMJ <andrejoao070@gmail.com>
2024-05-25 00:41:09 +0100

    UPDATE commented unnecessary code

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 const App = React.lazy(()=> import("./App.jsx"))
-const UnderConst = React.lazy(()=> import("./underConstruction/UnderConst.jsx"))
+//const UnderConst = React.lazy(()=> import("./underConstruction/UnderConst.jsx"))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
